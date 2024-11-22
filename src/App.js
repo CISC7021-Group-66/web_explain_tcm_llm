47272d397619c64b42311ecd0818f73d5b6ca887
Rookie, LIN <1049825685@qq.com>
2024-11-22 11:17:26 +0800

    :art: 根據輸入框來進行提問

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import { useEffect } from "react";
+import { useState } from "react";
 
 function App() {
+  const [inputValue, setInputValue] = useState("张某，男，27岁。患者因昨晚饮酒发热，喝凉水数杯，早晨腹痛腹泻，大便如水色黄，腹中辘辘有声，恶心欲吐，胸中满闷不舒，口干欲冷饮，舌质红、苔白腻，脉沉细数。给出中医诊断和处方建议。");
 
   async function fetchStream() {
+    // 修改此處ip為server ip
     const response = await fetch("http://100.96.0.5:8000/fullQuery", {
         method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ question: "张某，男，27岁。患者因昨晚饮酒发热，喝凉水数杯，早晨腹痛腹泻，大便如水色黄，腹中辘辘有声，恶心欲吐，胸中满闷不舒，口干欲冷饮，舌质红、苔白腻，脉沉细数。给出中医诊断和处方建议。" })
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ question: inputValue })
     });
 
     const reader = response.body.getReader();
@@ -52,14 +52,24 @@ function App() {
     }
   }
 
-
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-3xl font-bold text-blue-500">
-        Hello, Tailwind CSS!
-      </h1>
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+      <h1 className="text-2xl font-bold mb-4">中醫藥大語言模型可解釋分析系統</h1>
+      <textarea
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        className="mb-4 p-2 border border-gray-300 rounded resize"
+        placeholder="Enter your question"
+        rows="5"
+        cols="50"
+      />
 
-      <button onClick={fetchStream}>Fetch Data</button>
+      <button
+        onClick={() => fetchStream(inputValue)}
+        className="p-2 bg-blue-500 text-white rounded"
+      >
+        完整提問
+      </button>
     </div>
   );
 }
